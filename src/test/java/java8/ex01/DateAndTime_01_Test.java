7beeb9a68639015b0eb346a8efc30c6aefdd923e
Corentin Torres <torres.corentin@gmail.com>
2018-06-13 17:04:47 +0200

    exercice 1 terminé: manipulation des Dates et Calendar (avant java 8)

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -17,14 +17,13 @@ public class DateAndTime_01_Test {
     @Test
     public void test_date() {
 
-        // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(117, 2, 24, 1, 2, 3);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
-        // TODO modifier le jour de la date pour que le test soit passant
-
+        date.setDate(date.getDate()+1);
+        //date.setMonth(date.getMonth()+1);
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
@@ -34,13 +33,13 @@ public class DateAndTime_01_Test {
     public void test_calendar() throws Exception {
         Calendar calendar = Calendar.getInstance();
 
-        // TODO modifier l'objet calendar pour que le test soit passant
+        calendar.setTime(new Date(117, 2, 24, 1, 2, 3));
 
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
-
+        calendar.setTime(new Date(117, 2, 25, 1, 2, 3));
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
