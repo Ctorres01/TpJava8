236c7f6af9fe9bb56b2e4bde422515e188835a80
Corentin Torres <torres.corentin@gmail.com>
2018-06-14 11:13:47 +0200

    Exercice 2 terminé: utilisation de BiFunction

--- src/test/java/java8/ex02/Function_02_Test.java
+++ src/test/java/java8/ex02/Function_02_Test.java
@@ -12,17 +12,13 @@ import java.util.function.BiFunction;
 public class Function_02_Test {
 
     //  tag::buildAccount[]
-    // TODO Compléter la fonction buildAccount
-    // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount = (p, i) -> new Account(p, i);
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
-
-        // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
-
+    	
+        Account account = buildAccount.apply(new Person("John", "France", 80, "pass"), 500);
         assert account.getBalance().equals(500);
         assert account.getOwner().getFirstname().equals("John");
         assert account.getOwner().getLastname().equals("France");
