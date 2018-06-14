901dfd20af535af36237c62c6786f4620e911e36
Corentin Torres <torres.corentin@gmail.com>
2018-06-14 11:46:22 +0200

    Exercice 5 terminé: utilisation de Consumer

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -2,6 +2,8 @@ package java8.ex05;
 
 import java8.data.Data;
 import java8.data.Person;
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import java.util.List;
@@ -13,33 +15,16 @@ import java.util.function.Consumer;
 public class Function_05_Test {
 
     //tag::functions[]
-    // TODO compléter la fonction
-    // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
-
-    // TODO compléter la fonction
-    // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
-
-    // TODO compléter la fonction
-    // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> changePasswordToSecret = p -> p.setPassword("secret");
+    Consumer<Person> verifyAge = p -> Assert.assertTrue(p.getAge()>4);
+    Consumer<Person> verifyPassword = p -> Assert.assertEquals("secret", p.getPassword());
     //end::functions[]
 
 
     @Test
     public void test_consumer() throws Exception {
         List<Person> personList = Data.buildPersonList();
-
-        // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
-        // personList.forEach...
-
-        // TODO remplacer la boucle for par l'invocation de la méthode forEach
-        // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
-        // personList.forEach...
-        for(Person p : personList) {
-            verifyAge.accept(p);
-            verifyPassword.accept(p);
-        }
+        personList.forEach(p -> changePasswordToSecret.accept(p));
+        personList.forEach(p -> verifyAge.andThen(verifyPassword).accept(p));
     }
 }
