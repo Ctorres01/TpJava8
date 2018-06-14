234e6ce9a62085d1e4d68b54ad0eaa32c57f9eaa
Corentin Torres <torres.corentin@gmail.com>
2018-06-14 16:44:03 +0200

    Exercice deux terminé: map + flatMap

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,8 +24,7 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
-
+        List<Customer> result = orders.stream().map(Order::getCustomer).distinct().collect(Collectors.toList());
         assertThat(result, hasSize(2));
     }
 
@@ -34,10 +33,7 @@ public class Stream_02_Test {
 
         List<Order> orders = new Data().getOrders();
 
-        // TODO calculer les statistiques sur les prix des pizzas vendues
-        // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
-
+        IntSummaryStatistics result = orders.stream().flatMap(o -> o.getPizzas().stream()).collect(Collectors.summarizingInt(Pizza::getPrice));
 
         assertThat(result.getSum(), is(10900L));
         assertThat(result.getMin(), is(1000));
